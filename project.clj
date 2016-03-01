e3bf25d026baa03ce724f8503f0ec907de6cbd45
mrmcc3 <mrmcc3@gmail.com>
2016-03-01 20:02:14 +1000

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mrmcc3/firebase-clj "0.1.0"
+(defproject mrmcc3/firebase-clj "0.1.1-SNAPSHOT"
   :description "Minimalist Clojure[Script] Firebase Wrapper"
   :url "http://github.com/mrmcc3/firebase-clj"
   :license {:name "Eclipse Public License"
