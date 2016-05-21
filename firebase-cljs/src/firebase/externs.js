094a9d8defea3a8d8ceaa85d7c2dbe3479ad0493
Michael McClintock <mrmcc3@gmail.com>
2016-05-22 08:50:34 +1000

    add more externs

--- firebase-cljs/src/firebase/externs.js
+++ firebase-cljs/src/firebase/externs.js
@@ -12,7 +12,7 @@ window.firebase.SDK_VERSION = "";
 window.firebase.initializeApp = function(name) {};
 
 // --------------------------------------------------------------------------------
-// https://firebase.google.com/docs/reference/js/firebase.app.App
+// App -> https://firebase.google.com/docs/reference/js/firebase.app.App
 var app = {};
 app.name = "";
 app.options = {};
@@ -22,16 +22,28 @@ app.delete = function() {};
 app.storage = function() {};
 
 // --------------------------------------------------------------------------------
-// https://firebase.google.com/docs/reference/js/firebase.auth.Auth
+// Auth -> https://firebase.google.com/docs/reference/js/firebase.auth.Auth
 var auth = {};
 auth.app = {};
 auth.currentUser = null;
+auth.applyActionCode = function(code) {};
+auth.checkActionCode = function(code) {};
+auth.confirmPasswordReset = function(code, newPassword) {};
+auth.createUserWithEmailAndPassword = function(email, password) {};
+auth.fetchProvidersForEmail = function(email) {};
+auth.getRedirectResult = function() {};
+auth.onAuthStateChanged = function(nextOrObserver, opt_error, opt_completed) {};
+auth.signInAnonymously = function() {};
+auth.signInWithCredential = function(credential) {};
+auth.signInWithCustomToken = function(token) {};
+auth.signInWithEmailAndPassword = function(email, password) {};
+auth.signInWithPopup = function(provider) {};
+auth.signInWithRedirect = function(provider) {};
 auth.signOut = function() {};
-auth.signInWithEmailAndPassword = function() {};
-// INCOMPLETE
+auth.verifyPasswordResetCode = function(code) {};
 
 // --------------------------------------------------------------------------------
-// https://firebase.google.com/docs/reference/js/firebase.User
+// User -> https://firebase.google.com/docs/reference/js/firebase.User
 var user = {};
 user.displayName = null;
 user.email = null;
@@ -56,10 +68,39 @@ user.updatePassword = function(newPassword) {};
 user.updateProfile = function(profile) {};
 
 // --------------------------------------------------------------------------------
-// https://firebase.google.com/docs/reference/js/firebase.database.Database
+// Database -> https://firebase.google.com/docs/reference/js/firebase.database.Database
 var database = {};
 database.ref = function(path) {};
 database.refFromUrl = function(url) {};
 database.goOffline = function() {};
 database.goOnline = function() {};
 
+// --------------------------------------------------------------------------------
+// Reference -> https://firebase.google.com/docs/reference/js/firebase.database.Reference
+var reference = {};
+reference.key = "";
+reference.parent = {};
+reference.root = {};
+reference.ref = {};
+reference.child = function(path) {};
+reference.endAt = function(value, key) {};
+reference.equalTo = function(value, key) {};
+reference.limitToFirst = function(limit) {};
+reference.limitToLast = function(limit) {};
+reference.off = function(eventType, callback, context) {};
+reference.on = function(eventType, callback, cancelCallbackOrContext, context) {};
+reference.once = function(eventType, userCallback) {};
+reference.onDisconnect = function() {};
+reference.orderByChild = function(path) {};
+reference.orderByKey = function() {};
+reference.orderByPriority = function() {};
+reference.orderByValue = function() {};
+reference.push = function(value, onComplete) {};
+reference.remove = function(onComplete) {};
+reference.set = function(newVal, onComplete) {};
+reference.setPriority = function(priority, onComplete) {};
+reference.setWithPriority = function(newVal, newPriority, onComplete) {};
+reference.startAt = function(value, key) {};
+reference.toString = function() {};
+reference.transaction = function(transactionUpdate, onComplete, applyLocally) {};
+reference.update = function(objectToMerge, onComplete) {};
