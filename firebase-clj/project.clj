6cdea320c66397d21c9089976d5360848969bd42
Michael McClintock <mrmcc3@gmail.com>
2016-07-05 14:30:10 +1000

    bump firebase dep

--- firebase-clj/project.clj
+++ firebase-clj/project.clj
@@ -5,4 +5,4 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [com.google.firebase/firebase-server-sdk "3.0.0"]])
+                 [com.google.firebase/firebase-server-sdk "3.0.1"]])
